f22d981057eacb79c2cbe899e058d108ef4c5bf6
firkax-new-desk <157710697+Firkax69@users.noreply.github.com>
2024-08-09 10:10:29 +0100

    fix: shuffle fucntion and lost message appear feat: add setTimeout on message and style changes

--- src/assets/ContainerCards.jsx
+++ src/assets/ContainerCards.jsx
@@ -3,7 +3,7 @@ import Cards from "./Cards";
 
 function shuffleArray(array) {
     const shuffledArray = array.slice();
-    for (let i = shuffleArray.length - 1; i > 0; i--) {
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
         const randomIndex = Math.floor(Math.random() * (i+1));
 
         [shuffledArray[i], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[i],];
@@ -12,18 +12,32 @@ function shuffleArray(array) {
 }
 
 function ContainerCards() {
-    const [pokemonIds, setPokemonIds] = useState([134, 135, 136, 196, 197, 470, 471, 700, 133]);
+    const [pokemonIds, setPokemonIds] = useState([130, 131, 139, 198, 192, 473, 475, 708, 132]);
     const [score, setScore] = useState(0);
     const [prevCard, setPrevCard] = useState(null);
     const [highestScore, setHighestScore] = useState(0);
+    const [lost, setLost] = useState(false);
 
     useEffect(() => {setPokemonIds((prevIds) => shuffleArray(prevIds))}, []);
+
+    useEffect(() => {
+       if (lost) {
+        const timer = setTimeout(() => {
+            setLost(false);
+        }, 4000); // 4 seconds delay
+
+       // Cleanup the timer on component unmount or if lost changes
+        return () => clearTimeout(timer);
+       }
+    }, [lost])
 
     const handleCardClick = (id) => {
         if(prevCard !== id) {
             setPrevCard(id);
-            setScore((prevScore) => prevScore + 1)
+            setScore((prevScore) => prevScore + 1);
+            setLost(false); // Reset lost state if the player is correct
         } else {
+            setLost(true); // Set lost to true when the same card is clicked
             if (score > highestScore) {
                 setHighestScore(score);
             }
@@ -34,14 +48,13 @@ function ContainerCards() {
         setPokemonIds((prevIds) => shuffleArray(prevIds))
     };
 
-    // Need to add CSS below
     return (
         <div className="flex flex-col items-center mt-8">
             <div className="flex justify-center mt-4 mb-4">
                 <h2 className="text-2xl font-bold">Score: {score}</h2>
             </div>
 
-            {prevCard !== null && score === 0 && (<div className="text-red-500 font-bold">You lost. Try again!</div>)}
+            {lost && (<div className="text-red-500 text-3xl font-extrabold animate-pulse">You lost. Try again!</div>)}
 
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
                 {pokemonIds.map((id) => (
